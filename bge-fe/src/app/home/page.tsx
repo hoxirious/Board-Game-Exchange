02c701155f4170645453230ce5b69f0735ca3bd8
Hy Huynh <giahy33@gmail.com>
2024-03-14 13:14:03 -0600

    feat: init landing page

--- bge-fe/src/app/home/page.tsx
+++ bge-fe/src/app/home/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button"
+
 const page = () => {
     return (
-        <div>
-            Home Page
+        <div className="h-screen bg-black grid sm:grid-cols-1 sm:gap-1">
+            <div className="flex flex-col  justify-center items-center">
+                <p className="text-white text-lg">Sharing is Caring!</p>
+                <p className="text-white text-sm">Share and gain new experiences from others by trading your old games at Board Game Exchange.</p>
+            </div>
+            <div className="grid bg-white grid-cols-2"> </div>
+            <Button className="w-36" variant="default" size="sm">Start Trading Now!</Button>
         </div>
     );
 }
