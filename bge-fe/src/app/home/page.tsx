bbeb572b8dff52f7a5170a3109592eb879ed3cbc
Rupert Amodia <rupertraphael.amodia@ucalgary.ca>
2024-03-25 17:14:09 +0000

    feat(ui): implement search with filters

--- bge-fe/src/app/home/page.tsx
+++ bge-fe/src/app/home/page.tsx
@@ -1,6 +1,16 @@
+import { BGEAdvancedSearchComponent } from "@/components/bge-advanced-search"
+
 const page = () => {
     return (
-        <div>Home</div>
+        <div>
+            Home Page
+            <section className="mx-auto md:w-[600px] lg:w-[1024px]">
+                <input className="w-full p-2 text-base border rounded" placeholder="Search"></input>
+                <div className="absolute inset-0 md:relative">
+                    <BGEAdvancedSearchComponent/>
+                </div>
+            </section>
+        </div>
     );
 }
 
--- bge-fe/src/lib/utils.ts
+++ bge-fe/src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function debounce(callback, delay) {
+  let timer = null;
+  return function() {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      callback();
+    }, delay)
+  }
+}
