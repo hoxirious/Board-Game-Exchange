8ea42d066872f9fedcfc4722bcecf1514394b164
Doan Hao Nguyen <haodnguyen2601@gmail.com>
2024-03-28 10:50:30 -0600

    todo: add bge-advanced-search
    Add BGEAdvanceSearch component to the filter button as popover

--- bge-fe/src/app/listingView/page.tsx
+++ bge-fe/src/app/listingView/page.tsx
@@ -6,8 +6,16 @@ import { SearchIcon, FilterIcon } from "lucide-react";
 import { GameCover } from "@/components/gameCover";
 import { listOfGames } from "./data/games";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { useState } from "react";
+import { BGEAdvancedSearchComponent } from "@/components/bge-advanced-search";
+import { 
+    Popover,
+    PopoverContent,
+    PopoverTrigger
+} from "@/components/ui/popover";
 
 const page = () => {
+
     return (
         <section className="flex flex-col items-center bg-gray-100">
             <div className="h-16 w-full bg-gray-400">    
@@ -15,9 +23,18 @@ const page = () => {
             <div className="flex w-full space-x-2 p-8 md:px-96">
                 <SearchIcon className="w-5 h-5 absolute ml-3.5 mt-2.5 text-primary-200"/>
                 <Input type="search" placeholder="Type a command or search..." className="pr-3 pl-8"/>
-                <Button type="submit" variant="outline" className="text-primary-300 hover:text-primary-400">
-                    <FilterIcon />
-                </Button>
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <Button type="submit" variant="outline" className="text-primary-300 hover:text-primary-400">
+                            <FilterIcon />
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-screen">
+                        <ScrollArea className="h-[30rem]">
+                            <BGEAdvancedSearchComponent />
+                        </ScrollArea>
+                    </PopoverContent>
+                </Popover>
             </div>
             <ScrollArea className="h-[32.9rem]">
                 <div className="grid grid-cols-2 md:grid-cols-4 gap-y-4 gap-x-6 px-4">
