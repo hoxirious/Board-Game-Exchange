b88a5874ec365f9b18e78ea39bec346bcf1455e2
Hy Huynh <giahy33@gmail.com>
2024-04-06 11:28:23 -0600

    chore: add game listing into Landing Page

--- bge-fe/src/components/gameCover.tsx
+++ bge-fe/src/components/gameCover.tsx
@@ -3,28 +3,32 @@ import { cn } from "@/lib/utils";
 import { Game } from "@/app/listingView/data/games";
 
 interface GameCoverProps extends React.HTMLAttributes<HTMLDivElement> {
-    game: Game
+    game: Game,
+    gameNameClassName?: string,
+    gameLocationClassName?: string,
 }
 
 export function GameCover({
     game,
     className,
+    gameNameClassName,
+    gameLocationClassName,
     ...props
 }: GameCoverProps) {
     return (
-        <section className={cn("space-y-3", className)} {...props}>
+        <section className={cn("", className)} {...props}>
             <div>
-                <Image 
-                src={game.cover}
-                alt={game.name}
-                width={200}
-                height={200}
-                className={cn("rounded-2xl transition-all hover:scale-105")}
+                <Image
+                    src={game.cover}
+                    alt={game.name}
+                    width={200}
+                    height={200}
+                    className="rounded-2xl mx-auto transition-all hover:scale-105"
                 />
             </div>
             <div className="space-y-1 text-sm">
-                <h3 className="font-medium leading-none">{game.name}</h3>
-                <p className="text-xs text-muted-foreground">{game.location}</p>
+                <h3 className={cn("font-medium leading-none", gameNameClassName)}>{game.name}</h3>
+                <p className={cn("text-xs text-muted-foreground", gameLocationClassName)}>{game.location}</p>
             </div>
         </section>
     )
