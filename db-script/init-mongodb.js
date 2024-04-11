203db50277ba470eb78d5d790dc34680054c1b73
Amnah Hussain <73035499+Amnah736@users.noreply.github.com>
2024-04-10 23:14:36 -0600

    chore: multiple backend fixes
    - seeding posts with variety of data rather than repeated
    - seeding data of various times
    - ordering messages sent back from message route (oldest to newest)
    - remove session creation on signup in backend

--- db-script/init-mongodb.js
+++ db-script/init-mongodb.js
@@ -101,6 +101,89 @@ async function createCollectionWithValidator(db, name, schema) {
     console.log(`${name} collection created`);
 }
 
+const seedPosts = [
+    {
+        "title": "Catan BoardGame",
+        "image": "https://cf.geekdo-images.com/original/img/A-0yDJkve0avEicYQ4HoNO-HkK8=/0x0/pic2419375.jpg",
+        "category": "Economic",
+        "condition": "New",
+        "location": "Calgary, AB",
+        "description": "In CATAN, players try to be the dominant force on the island of Catan by building settlements, cities, and roads."
+    },
+    {
+        "title": "Carcassonne",
+        "image": "https://cf.geekdo-images.com/original/img/o4p6f88SGE899BTNMzTvERVWZ-M=/0x0/pic2337577.jpg",
+        "category": "Medieval",
+        "condition": "Used - Like New",
+        "location": "Edmonton, AB",
+        "description": "It is a tile-placement game in which the players draw and place a tile with a piece of southern French landscape on it."
+    },
+    {
+        "title": "Pandemic",
+        "image": "https://cf.geekdo-images.com/original/img/j-pfXZ_0GmOowohzD_T6NDAWGSA=/0x0/pic1534148.jpg",
+        "category": "Strategy",
+        "condition": "Used - Good",
+        "location": "Banff, AB",
+        "description": "In Pandemic, several virulent diseases have broken out simultaneously all over the world! The players are disease-fighting specialists whose mission is to treat disease hotspots while researching cures for each of four plagues before they get out of hand."
+    },
+    {
+        "title": "7 Wonders Board Game",
+        "image": "https://cf.geekdo-images.com/original/img/3DP_RW5lTX0WrV67s8qi8CsiXoQ=/0x0/pic860217.jpg",
+        "category": "Economic",
+        "condition": "Used - Fair",
+        "location": "Canmore, AB",
+        "description": "You are the leader of one of the 7 great cities of the Ancient World. Gather resources, develop commercial routes, and affirm your military supremacy. Build your city and erect an architectural wonder which will transcend future times."
+    },
+    {
+        "title": "Dominion",
+        "image": "https://cf.geekdo-images.com/original/img/oN8CHUZ8CF6P1dFnhMCJXvE8SOk=/0x0/pic394356.jpg",
+        "category": "Card Game",
+        "condition": "Used - Poor",
+        "location": "Vancouver, BC",
+        "description": "You are a monarch, like your parents before you, a ruler of a small pleasant kingdom of rivers and evergreens. Unlike your parents, however, you have hopes and dreams! You want a bigger and more pleasant kingdom, with more rivers and a wider variety of trees. You want a Dominion!"
+    },
+    {
+        "title": "Ticket to Ride",
+        "image": "https://cf.geekdo-images.com/original/img/bOcAJxw-W-B2Gz0x67z8bjqzJgY=/0x0/pic38668.jpg",
+        "category": "Exploration",
+        "condition": "New",
+        "location": "Whistler, BC",
+        "description": "With elegantly simple gameplay, Ticket to Ride can be learned in under 15 minutes. Players collect cards of various types of train cars they then use to claim railway routes in North America. The longer the routes, the more points they earn."
+    },
+    {
+        "title": "Codenames BoardGame",
+        "image": "https://cf.geekdo-images.com/original/img/MJ6MNz8-_rb1C2VUTnYBTYOiiy0=/0x0/pic2582929.jpg",
+        "category": "Party Game",
+        "condition": "Used - Like New",
+        "location": "Victoria, BC",
+        "description": "In Codenames, two teams compete to see who can make contact with all of their agents first. Lay out 25 cards, each bearing a single word. The spymasters look at a card showing the identity of each card, then take turns clueing their teammates. "
+    },
+    {
+        "title": "Agricola",
+        "image": "https://cf.geekdo-images.com/original/img/WQS7FRLHzvFQ8mjt94Au4W8CYn0=/0x0/pic831744.jpg",
+        "category": "Economic",
+        "condition": "Used - Good",
+        "location": "Kamloops, BC",
+        "description": "In Agricola, you're a farmer in a wooden shack with your spouse and little else. On a turn, you get to take only two actions, one for you and one for the spouse, from all the possibilities you'll find on a farm: collecting clay, wood, or stone; building fences; and so on. "
+    },
+    {
+        "title": "Puerto Rico",
+        "image": "https://cf.geekdo-images.com/original/img/RvdWBkmDks7AN8sodR4PyVcponA=/0x0/pic158548.jpg",
+        "category": "Economic",
+        "condition": "Used - Fair",
+        "location": "Kelowna, BC",
+        "description": "In Puerto Rico, players assume the roles of colonial governors on the island of Puerto Rico. The aim of the game is to amass victory points by shipping goods to Europe or by constructing buildings."
+    },
+    {
+        "title": "Small World Board Game",
+        "image": "https://cf.geekdo-images.com/original/img/WvXe0YG-Mj2KBtyRSBNtRw4DIx0=/0x0/pic428828.jpg",
+        "category": "Fantasy",
+        "condition": "Used - Poor",
+        "location": "Red Deer, AB",
+        "description": "In Small World, players vie for conquest and control of a world that is simply too small to accommodate them all."
+    }
+]
+
 async function seedUserAndPostsAndMessages() {
     let users = [];
     for (let i = 1; i <= 2; i++) {
@@ -124,14 +207,14 @@ async function seedUserAndPostsAndMessages() {
     let posts = [];
     for (let i = 1; i <= 10; i++) {
         posts.push({
-            postsPictureUrl: ["https://cf.geekdo-images.com/original/img/A-0yDJkve0avEicYQ4HoNO-HkK8=/0x0/pic2419375.jpg"],
-            title: `BoardGame${i}`,
-            description: `This is a test description`,
-            location: "Test Location",
-            condition: "Used - Like New",
-            category: "Card Game",
+            postsPictureUrl: [seedPosts[i-1].image],
+            title: seedPosts[i-1].title,
+            description: seedPosts[i-1].description,
+            location: seedPosts[i-1].location,
+            condition: seedPosts[i-1].condition,
+            category: seedPosts[i-1].category,
             ownerUserID: userId1,
-            dateCreated: new Date(Date.now())
+            dateCreated: new Date(Date.now() - (i*86400000))
         })
     }
 
@@ -142,7 +225,7 @@ async function seedUserAndPostsAndMessages() {
     let messages = [];
     for (let i = 1; i <= 4; i++) {
         messages.push({
-            timestamp: new Date(Date.now()),
+            timestamp: new Date(Date.now() + (1000*i)),
             text: `first: message ${i}`,
             postId: postId,
             senderUserID: userId2,
@@ -151,7 +234,7 @@ async function seedUserAndPostsAndMessages() {
         })
 
         messages.push({
-            timestamp: new Date(Date.now()),
+            timestamp: new Date(Date.now() + (1500*i)),
             text: `reply: message ${i}`,
             postId: postId,
             senderUserID: userId1,
